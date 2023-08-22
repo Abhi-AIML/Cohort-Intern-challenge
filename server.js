3a942659984a08ef614f0b357c130003ed9eec00
Abhishek S <136588531+Abhi-AIML@users.noreply.github.com>
2023-08-23 00:51:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./src/routes/authRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/', authRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
